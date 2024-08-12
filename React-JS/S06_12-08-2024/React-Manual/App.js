6ba7d24185a632033ff56d20e35f67a4cf39690c
Kabir <sagithedeveloper@gmail.com>
2024-08-12 16:18:44 +0530

    added the code

---
+++ React-JS/S06_12-08-2024/React-Manual/App.js
@@ -0,0 +1,15 @@
+import Header from "./Header.js";
+import { Main } from "./Main.js";
+import Footer from "./Footer.js";
+
+function App() {
+  return (
+    <div>
+      <h2>App</h2>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+export default App;
