66c5b0610e3dda267bad5c16ea57b4eef7646838
Kabir <sagithedeveloper@gmail.com>
2024-09-23 10:33:03 +0530

    Added the code

---
+++ React-JS/S28_23-09-2024/src/components/Header.jsx
@@ -0,0 +1,6 @@
+import "./Header.css";
+function Header() {
+  return <div className="header">Header</div>;
+}
+
+export default Header;
---
+++ React-JS/S28_23-09-2024/src/components/Main.jsx
@@ -0,0 +1,47 @@
+var h2Styles = {
+  color: "green",
+};
+function Main() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <h2 style={h2Styles}>React Concepts</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim beatae
+        fuga perferendis accusamus et saepe cupiditate laboriosam porro fugit
+        est nam voluptates eveniet pariatur, quaerat recusandae rem ea dolor
+        magni. Accusantium exercitationem, explicabo laboriosam fugiat molestias
+        id cum necessitatibus error commodi ullam dolorum consequatur at aperiam
+        totam eligendi laudantium dolor a modi eius. Velit quisquam magnam
+        inventore ab aperiam rem hic expedita in ad. Iste, fugiat laborum quidem
+        reprehenderit accusamus maxime? Saepe sint ad quos quo nulla natus
+        dolorem facere unde error, adipisci consequuntur nobis ullam voluptatum
+        quibusdam asperiores consequatur molestiae iure. Illo necessitatibus
+        debitis fugit eveniet commodi incidunt? Eius assumenda inventore animi
+        repudiandae maxime, quidem earum quae, iusto eum iste quas autem
+        possimus quaerat sequi, accusamus qui sit culpa adipisci debitis? Dicta
+        expedita fugit suscipit sit accusamus iste numquam fuga. Aut voluptas
+        atque reprehenderit temporibus ipsum, minima magnam? Odit assumenda, ab
+        illo magni reiciendis eius ipsum dolore ad laboriosam temporibus minima
+        praesentium asperiores molestiae quisquam accusantium animi culpa,
+        eveniet quibusdam voluptas suscipit nesciunt maiores? Maxime error
+        aliquam magnam iste laudantium at quis culpa! Repellat suscipit omnis
+        quia, autem illum quasi velit dolore, in veniam nisi aliquid tempore
+        soluta totam eligendi consequatur dicta, quas ad. Quidem aspernatur
+        illum labore placeat repellat debitis, quos nostrum nulla ratione
+        repellendus eos magni perspiciatis quo animi aut itaque ullam. Eaque
+        doloremque minus nostrum temporibus officiis. Id molestiae recusandae
+        dolorum dignissimos, voluptas enim libero iure in atque dolore ab
+        eveniet facilis itaque minima incidunt, tempora quis ipsam at culpa quia
+        earum consequatur veritatis? Doloribus, error dignissimos? Ipsam
+        accusantium corporis aut ipsa, facere ipsum fuga excepturi cum sint
+        autem voluptate non reiciendis sit soluta quia deleniti, placeat
+        laboriosam. Dolor odio dolores id reiciendis voluptates vero praesentium
+        ea quia iusto. Esse, consequuntur nihil? Tenetur libero dolore voluptate
+        a reiciendis quos totam pariatur, recusandae illo voluptatum, neque
+        nesciunt?
+      </p>
+    </div>
+  );
+}
+
+export default Main;
